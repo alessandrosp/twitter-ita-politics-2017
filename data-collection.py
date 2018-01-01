3b9f3ea10f9734e5534b8c2564dd7cb4b023e1f9
Alessandro Scoccia Pappagallo <alessandrosp@alessandrosp-macbookpro.roam.corp.google.com>
2018-01-01 18:58:47 +0100

    First commit

---
+++ data-collection.py
@@ -0,0 +1,76 @@
+import datetime
+import re
+
+import tinydb
+import tweepy
+
+_CONSUMER_KEY = 'sUEjiQIVm7NL13z1rFTPw'
+_CONSUKER_SECRET = 'Iwdawl261pQrC9ybxf3dt4mmskow6rhhz1P69W565A'
+_ACCESS_TOKEN = '341464649-nP5kNGthf7x1QQNs5ebzWuBSzzMOeVWHEfTYKAPQ'
+_ACCESS_TOKEN_SECRET = 'QLokF3u5iG2siSxiaY26xYfjwWunJ94FZlpFUQ5YCs8lL'
+
+_MIN_DATE = datetime.datetime(2017,  1,  1,  0,  0,  0)  # inclusive
+_MAX_DATE = datetime.datetime(2017, 12, 24, 23, 59, 59)  # inclusive
+_MIN_TWEET_ID = 796192757836554240  # exclusive
+_MAX_TWEET_ID = 946071696200650759  # inclusive
+# test 823490915541143552
+# prod 946071696200650759
+
+_CANDIDATES = [
+	'PietroGrasso',  # Pietro Grasso
+	'matteorenzi',  # Matteo Renzi
+	'luigidimaio',  # Luigi di Maio
+	'berlusconi',  # Silvio Berlusconi
+	'GiorgiaMeloni',  # Giorgia Meloni
+	'matteosalvinimi',  # Matteo Salvini
+	]
+
+def CreateTwitterConnection():
+	"""It uses authentication keys to connect to Twitter API."""
+	auth = tweepy.OAuthHandler(_CONSUMER_KEY, _CONSUKER_SECRET)
+	auth.set_access_token(_ACCESS_TOKEN, _ACCESS_TOKEN_SECRET)
+	return tweepy.API(auth)
+
+
+def GetTweets(connection, screen_name):
+	"""Get all the tweets for the user specified by screen_name."""
+	tweets = []
+	min_id = _MIN_TWEET_ID
+	max_id = _MAX_TWEET_ID
+	retweet_regex = re.compile(r'^(RT|rt)( @\w*)?[: ]')
+	done = False
+
+	while not done:
+		result_set = connection.user_timeline(
+			screen_name=screen_name,
+			count=200,
+			tweet_mode='extended',
+			since_id=min_id,
+			max_id=max_id)
+		if result_set:
+			max_id = min([tweet.id for tweet in result_set])-1
+			tweets += [tweet.full_text 
+					   for tweet in result_set
+					   if _MIN_DATE <= tweet.created_at <= _MAX_DATE
+					      and not retweet_regex.match(tweet.full_text)
+					   ]
+		else:
+			done = True
+
+	return tweets
+
+
+def SaveTweets(tweets, table_name):
+	"""Save the scraped tweets into a TinyDB table."""
+	table = db.table(table_name)
+	for tweet in tweets:
+		table.insert({'text': tweet})
+
+if __name__ == '__main__':
+	db_name = datetime.datetime.today().strftime('db/%Y%m%d%H%M.json')
+	db = tinydb.TinyDB(db_name)
+	connection = CreateTwitterConnection()
+	for candidate in _CANDIDATES:
+		print('Processing tweets for {}'.format(candidate))
+		tweets = GetTweets(connection, candidate)
+		SaveTweets(tweets, candidate)
