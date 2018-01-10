087c166ad58ed86894ee7d65a55f9eaf1f5e7bc8
Alessandro Scoccia Pappagallo <alessandro.scoccia.pappagallo@gmail.com>
2018-01-10 08:55:24 +0000

    Added last functions and output

--- descriptive_analysis.py
+++ descriptive_analysis.py
@@ -7,8 +7,10 @@ The statistics are then outputted to CSV.
 """
 
 import collections
+import copy
 import re
 
+import pandas as pd
 import tinydb
 
 _DB_NAME = 'db/201801011249.json'
@@ -16,13 +18,15 @@ _DB_NAME = 'db/201801011249.json'
 _CANDIDATES = [
     'PietroGrasso',  # Pietro Grasso
     'matteorenzi',  # Matteo Renzi
-    'luigidimaio',  # Luigi di Maio
-    'berlusconi',  # Silvio Berlusconi
-    'GiorgiaMeloni',  # Giorgia Meloni
-    'matteosalvinimi',  # Matteo Salvini
+    'luigidimaio',  # Luigi di Maio
+    'berlusconi',  # Silvio Berlusconi
+    'GiorgiaMeloni',  # Giorgia Meloni
+    'matteosalvinimi',  # Matteo Salvini
     ]
 
 _LINK_REGEX = re.compile(r'(https?|www)[^ ]+')
+_MENTION_REGEX = re.compile(r'@[^ ]+')
+_HASHTAG_REGEX = re.compile(r'#[^ ]+')
 
 
 def load_dataset():
@@ -65,25 +69,53 @@ def add_shortest_length(dataset):
     for candidate in _CANDIDATES:
         tweets = dataset[candidate]['tweets']
         longest_length = min([len(tweet) for tweet in tweets])
-        dataset[candidate]['longest_length'] = longest_length
+        dataset[candidate]['shortest_length'] = longest_length
 
 
-def add_ratio_tweets_with_links(dataset):
-    """It adds the ratio of tweets with links over the total number."""
+def add_number_of_links(dataset):
+    """It adds the number of links per candidate to dataset."""
     for candidate in _CANDIDATES:
+        count_links = 0
         tweets = dataset[candidate]['tweets']
-        num_tweets = len(dataset[candidate]['tweets'])
-        with_links = [1 if _LINK_REGEX.match(tweet) else 0
-                      for tweet in tweets]
-        dataset[candidate]['longest_length'] = sum(with_links) / num_tweets
+        for tweet in tweets:
+            count_links += len(_LINK_REGEX.findall(tweet))
+        dataset[candidate]['num_links'] = count_links
+
+
+def add_number_of_hashes(dataset):
+    """It adds the number of hashes per candidate to dataset."""
+    for candidate in _CANDIDATES:
+        count_hashes = 0
+        tweets = dataset[candidate]['tweets']
+        for tweet in tweets:
+            count_hashes += len(_HASHTAG_REGEX.findall(tweet))
+        dataset[candidate]['num_hashes'] = count_hashes
+
+
+def add_number_of_mentions(dataset):
+    """It adds the number of mentions per candidate to dataset."""
+    for candidate in _CANDIDATES:
+        count_mentions = 0
+        tweets = dataset[candidate]['tweets']
+        for tweet in tweets:
+            count_mentions += len(_MENTION_REGEX.findall(tweet))
+        dataset[candidate]['num_mentions'] = count_mentions
+
+
+def _remove_tweets(candidate_dict):
+    """It removes the key 'tweets' from the candidate_dict."""
+    new_dict = copy.deepcopy(candidate_dict)
+    del new_dict['tweets']
+    return new_dict
 
 
 def output_dataset(dataset):
-    """."""
-    # remove tweets
-    # cast as pandas
-    # tocsv
-    del dataset
+    """It strips the candidates' dicts of 'tweets' and then outputs to CSV."""
+    output = collections.defaultdict(dict)
+    for candidate in _CANDIDATES:
+        output[candidate] = _remove_tweets(dataset[candidate])
+    df_to_output = pd.DataFrame(output)
+    df_to_output.to_csv('descriptive_stats.csv')
 
 
 def main():
@@ -93,8 +125,9 @@ def main():
     add_average_length(dataset)
     add_longest_length(dataset)
     add_shortest_length(dataset)
-    add_ratio_tweets_with_links(dataset)
-
+    add_number_of_links(dataset)
+    add_number_of_hashes(dataset)
+    add_number_of_mentions(dataset)
     output_dataset(dataset)
 
 if __name__ == '__main__':
